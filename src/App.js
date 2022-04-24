27f900d30990b6abce4f4c52b2a12cece04aa09f
DavidGit999 <itprog999@gmail.com>
2022-04-24 15:40:41 +0300

    Create static movie card template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import API_KEY from './API_KEY';
 import './App.css';
+import MovieCard from './MovieCard';
 import SearchIcon from './search.svg'
 
 const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
 
+const movie1 = {
+  "Title": "Batman v Superman: Dawn of Justice",
+  "Year": "2016",
+  "imdbID": "tt2975590",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+}
+
 const App = () => {
+  const [movies, setMovies] = useState([])
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json()
@@ -16,7 +27,24 @@ const App = () => {
   }, [])
 
   return (
-    <h1>Movie search</h1>
+    <div className="app">
+      <h1>Movie search</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for any movie"
+          onChange={() => { }}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => { }} />
+      </div>
+
+      <div className="container">
+        <MovieCard movie1={movie1} />
+      </div>
+    </div>
   );
 }
 
