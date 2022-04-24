6f806cc0ef3cba65d4e44d986fa25753cbde024b
DavidGit999 <itprog999@gmail.com>
2022-04-24 16:15:57 +0300

    Search movie by clicking on icon

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SearchIcon from './search.svg'
 
 const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
 
-const movie1 = {
+const movie = {
   "Title": "Batman v Superman: Dawn of Justice",
   "Year": "2016",
   "imdbID": "tt2975590",
@@ -16,10 +16,13 @@ const movie1 = {
 
 const App = () => {
   const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json()
+
+    setMovies(data.Search)
   }
 
   useEffect(() => {
@@ -33,17 +36,30 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for any movie"
-          onChange={() => { }}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => { }} />
+          onClick={(e) => searchMovies(searchTerm)} />
       </div>
 
-      <div className="container">
-        <MovieCard movie1={movie1} />
-      </div>
+      {movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        )
+        : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
+
     </div>
   );
 }
