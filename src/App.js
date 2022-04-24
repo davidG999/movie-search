59774ade299198e9459b04069add387d5ae064c8
DavidGit999 <itprog999@gmail.com>
2022-04-24 12:48:28 +0300

    Get response data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import API_KEY from './API_KEY';
 
-const API_URL = `http://www.omdbapi.com/apikey=${API_KEY}`;
-console.log(API_URL)
+const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+  }
+
+  useEffect(() => {
+    searchMovies('superman')
+  }, [])
+
   return (
     <h1>Movie search</h1>
   );
