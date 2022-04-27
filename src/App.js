a2ed892283153e8977cbbe9cd2efe7bfe3e72c37
DavidGit999 <itprog999@gmail.com>
2022-04-27 22:08:21 +0300

    Create header

--- src/App.js
+++ src/App.js
@@ -1,63 +1,12 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import MovieCard from './MovieCard';
-import SearchIcon from './search.svg'
+import './index.css';
 
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
+import Header from './components/Header';
 
 const App = () => {
-  const [movies, setMovies] = useState([])
-  const [searchTerm, setSearchTerm] = useState('')
-
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`)
-    const data = await response.json()
-
-    setMovies(data.Search)
-  }
-
-  useEffect(() => {
-    searchMovies(searchTerm)
-  }, [searchTerm])
-
   return (
-    <div className="app">
-      <h1>Movie search</h1>
-
-      <div className="search">
-        <input
-          placeholder="Search for any movie"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          onKeyUpCapture={(e) => {
-            if (e.key === 'Enter') {
-              e.preventDefault()
-              e.target.nextElementSibling.click()
-            }
-          }}
-        />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={(e) => searchMovies(searchTerm)} />
-      </div>
-
-      {movies?.length > 0
-        ? (
-          <div className="container">
-            {movies.map((movie, i) => (
-              <MovieCard movie={movie} key={i} />
-            ))}
-          </div>
-        )
-        : (
-          <div className="empty">
-            <h2>No movies found</h2>
-          </div>
-        )
-      }
-
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-zinc-800 w-full flex justify-center py-4">
+      <span className="text-5xl text-slate-200">Movie search</span>
+    </div>
+  );
+}
+
+export default Header;
