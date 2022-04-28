ba4a987bd5aad29f36d68971cc188822ec688bb9
DavidGit999 <itprog999@gmail.com>
2022-04-28 13:38:35 +0300

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './index.css';
 
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <>
+    <div className="text-gray-300">
       <Header />
-    </>
+      <Navbar />
+    </div>
   );
 }
 
