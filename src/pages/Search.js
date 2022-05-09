4deb0fc81a1352fde45be0fa23762817b1234c71
DavidGit999 <itprog999@gmail.com>
2022-05-09 22:31:32 +0300

    Create search input box

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,6 +2,14 @@ const Search = () => {
   return (
     <div className="bg-zinc-800">
       <h2 className="text-slate-300 text-center uppercase font-extrabold p-1 tracking-wider">Search</h2>
+
+      <div class="flex justify-center">
+        <input
+          type="search" placeholder="Search..."
+          class="w-full text-gray-100 bg-gray-700 rounded transition ease-in-out p-3 m-4
+            focus:text-gray-200 focus:bg-gray-600 focus:outline-none"
+        />
+      </div>
     </div>
   );
 }
