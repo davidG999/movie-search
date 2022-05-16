88416071adcd7dfaab78fa48522a43e69547f1d0
DavidGit999 <itprog999@gmail.com>
2022-05-16 20:30:47 +0300

    Add an option to switch trending time period

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -6,9 +6,10 @@ import Pagination from './../components/Pagination';
 const Trending = () => {
   const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
+  const [time, setTime] = useState('day');
 
   const fetchTrending = async () => {
-    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`)
+    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/${time}?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`)
 
     setContent(data.results)
   }
@@ -16,11 +17,31 @@ const Trending = () => {
   useEffect(() => {
     fetchTrending()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
+  }, [time, page]);
 
   return (
     <div className="">
-      <h2 className="text-slate-300 text-center uppercase font-extrabold tracking-wider p-1 pb-3">Trending</h2>
+      <h2 className="text-slate-300 text-center uppercase font-extrabold tracking-wider p-1 pb-3 transition duration-500 ease-out">Trending</h2>
+
+      <ul className="p-1 m-3 flex justify-center text-center font-semibold text-gray-100 select-none">
+        <li
+          onClick={() => {
+            setTime('day');
+            setPage(1);
+          }}
+          className={`mr-2 cursor-pointer p-4 border border-solid border-slate-600 rounded hover:scale-103
+          ${time === 'day' ? 'bg-slate-600' : ''}
+          `}> Today </li>
+        <li
+          onClick={() => {
+            setTime('week');
+            setPage(1);
+          }}
+          className={`mr-2 cursor-pointer p-4 border border-solid border-slate-600 rounded hover:scale-103
+          ${time === 'week' ? 'bg-slate-600' : ''}
+          `}> This week </li>
+      </ul>
+
       <div className="flex flex-wrap justify-evenly w-2/3 my-0 mx-auto h-full">
         {content?.map((m) => {
           return (
