8cd9e263a4707ece7ae9d198acb30d72b8566b01
DavidGit999 <itprog999@gmail.com>
2022-05-19 21:04:22 +0300

    Change the style of single content cards
    Added:
    - underline and full title tooltip on hover
    - more rating info and colors

--- src/components/SingleContentCard.js
+++ src/components/SingleContentCard.js
@@ -1,28 +1,46 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import posterUnavailable from '../assets/TMDB/poster-unavailbable.jpg'
 import { p_300 } from '../assets/TMDB/posterSizes';
 
 const SingleContentCard = ({ id, poster, title, date, media_type, vote_average }) => {
+  const [underline, setUnderline] = useState('');
+
+  const ratingStyles =
+    vote_average >= 7 ? 'bg-green-600'
+      : vote_average >= 5 ? 'bg-orange-600'
+        : vote_average >= 0.1 ? 'bg-red-600'
+          : 'bg-blue-600 text-white w-10';
+
   return (
-    <Link to={id + media_type[0]} className="flex m-3">
-      <div className="relative bg-movie-card w-50 cursor-pointer hover:scale-103 duration-100 rounded-t-lg rounded">
+    <div className="flex m-3 text-slate-100">
+      <div className="relative bg-movie-card w-50 hover:bg-gray-600 hover:shadow-xl duration-100 rounded-t-lg rounded">
         <span className={
           `absolute inline-block w-8 h-7 font-medium rounded-full text-center p-1 -m-3 
-          ${vote_average >= 7 ? 'bg-high-rating' : 'bg-low-rating'}`
+          ${ratingStyles}`
         }>
-          {vote_average}
+          {vote_average || 'N/A'}
         </span>
-        <img src={`${p_300}/${poster}` || posterUnavailable} className="rounded-t" alt='Poster' />
-        <div className="text-center font-bold py-1 px-2">
-          {title}
-        </div>
+        <Link to={id + media_type[0]}>
+          <img src={`${p_300}/${poster}` || posterUnavailable} className="rounded-t" alt='Poster' />
+          <div
+            className={`text-center font-bold py-1 px-2 ${underline}`}
+            onMouseEnter={() => setUnderline('underline underline-offset-1 ')}
+            onMouseLeave={() => setUnderline('')}
+            title={title}
+          >
+            {title.length > 30 ? title.slice(0, 25) + '...' : title}
+          </div>
+        </Link>
+
         <div className="flex justify-between brightness-90 pt-2 px-2 pb-1">
-          <div> {media_type === 'movie' ? 'Movie' : 'TV Series'} </div>
-          <div className="bottom-0 left-0"> {date} </div>
+          <div> {media_type === 'movie' ? 'Movie' : 'TV'} </div>
+          <div className="bottom-0 left-0"> {date.split('-')[0]} </div>
+
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,6 @@ module.exports = {
     extend: {
       colors: {
         'movie-card': '#343a40',
-        'high-rating': '#3498db',
-        'low-rating': '#e74c3c',
       },
       width: {
         '50': '200px',
