28997b4b7fe6873c2fa1be1b0184533cbcaf0632
DavidGit999 <itprog999@gmail.com>
2022-05-27 23:29:24 +0300

    Add Deselect button in genres component

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -11,11 +11,8 @@ const GenreChip = ({ genreTitle, handleClick, isActive }) => {
         ${isActive ? 'bg-gray-300 text-gray-600 hover:bg-gray-400 hover:text-gray-700 removeIcons:hover:brightness-90'
           : 'bg-gray-600 text-gray-300 hover:bg-gray-500 hover:text-gray-200'}
       `}>
-
       {genreTitle}
-
       {isActive ? <img src={removeIcon} alt="Remove" className="transition duration-150 ease-in-out pl-1" /> : null}
-
     </span>
   );
 }
@@ -51,6 +48,11 @@ const Genres = ({
     setGenres(data.genres)
   }
 
+  const deselectAll = () => {
+    setSelectedGenres([])
+    fetchGenres()
+  }
+
   useEffect(() => {
     fetchGenres()
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -58,6 +60,17 @@ const Genres = ({
 
   return (
     <div className="flex flex-wrap justify-center space-x-2 mb-3 mx-32">
+      {selectedGenres.length > 0 &&
+        <>
+          <span
+            onClick={deselectAll}
+            className="px-2 py-1 my-1 rounded-full font-semibold text-sm flex align-center w-max cursor-pointer select-none transition duration-150 ease-in-out bg-red-500 text-gray-100 hover:bg-red-600">
+            Deselect all
+            <img src={removeIcon} alt="Remove" className="transition duration-150 ease-in-out pl-1" />
+          </span>
+          <span className='border border-gray-400'></span>
+        </>
+      }
 
       {selectedGenres?.map((genre) => {
         return (
