5acdb78315b4929b1a5b423a8e59757c0eabf8c7
DavidGit999 <itprog999@gmail.com>
2022-06-06 20:23:11 +0300

    Shorten names in single content cast info

--- src/pages/SingleContentInfo.js
+++ src/pages/SingleContentInfo.js
@@ -68,6 +68,11 @@ const SingleContentInfo = () => {
     }
   }
 
+  const shortenNameAndCharacter = (str) => {
+    if (str && str?.length > 15) return str.slice(0, 14) + '...'; // 18 is max, i guess
+    return str
+  }
+
   const ratingBg = vote_average >= 7 ? 'bg-green-600' : vote_average >= 5 ? 'bg-orange-600' : vote_average >= 0.1 ? 'bg-red-600' : 'bg-blue-600'
 
   function kFormatter(num) {
@@ -173,20 +178,20 @@ const SingleContentInfo = () => {
             </div>
           </div>
 
-          <div className='flex rounded shadow-2xl m-4 grow-0 pb-20'>
+          <div className='flex  rounded shadow-2xl m-4  pb-20'>
             {credits?.map((c) => (
-              <div key={c.id} className="rounded-lg bg-movie-card mr-2 last:mr-0">
+              <div key={c.id} className="w-full rounded-lg bg-movie-card mr-2 last:mr-0">
                 <img
                   className={`w-full rounded-t-lg ${!c.profile_path && 'w-200'}`}
                   src={c.profile_path ? `${credits.length > 5 ? p_500 : p_154}` + c.profile_path : noPicture} alt="Profile"
                 />
 
-                <div className="p-1 text-center font-medium">
-                  {c.name}
+                <div title={c.name} className="p-1 text-center font-medium">
+                  {c.name && shortenNameAndCharacter(c.name)}
                 </div>
 
-                <div className="p-2 opacity-80">
-                  as {c.character}
+                <div title={c.character} className="p-2 opacity-80">
+                  as {c.character && shortenNameAndCharacter(c.character)}
                 </div>
               </div>
             ))}
@@ -197,3 +202,184 @@ const SingleContentInfo = () => {
 }
 
 export default SingleContentInfo;
+
+// ! CONTENTS ----------------------------------
+
+// * MOVIE
+// {
+//   "adult": false,
+//   "backdrop_path": "/gG9fTyDL03fiKnOpf2tr01sncnt.jpg",
+//   "belongs_to_collection": null,
+//   "budget": 75000000,
+//   "genres": [
+//     {
+//       "id": 28,
+//       "name": "Action"
+//     },
+//     {
+//       "id": 878,
+//       "name": "Science Fiction"
+//     },
+//     {
+//       "id": 14,
+//       "name": "Fantasy"
+//     }
+//   ],
+//   "homepage": "https://www.morbius.movie/",
+//   "id": 526896,
+//   "imdb_id": "tt5108870",
+//   "original_language": "en",
+//   "original_title": "Morbius",
+//   "overview": "Dangerously ill with a rare blood disorder, and determined to save others suffering his same fate, Dr. Michael Morbius attempts a desperate gamble. What at first appears to be a radical success soon reveals itself to be a remedy potentially worse than the disease.",
+//   "popularity": 12394.971,
+//   "poster_path": "/6JjfSchsU6daXk2AKX8EEBjO3Fm.jpg",
+//   "production_companies": [
+//     {
+//       "id": 5,
+//       "logo_path": "/71BqEFAF4V3qjjMPCpLuyJFB9A.png",
+//       "name": "Columbia Pictures",
+//       "origin_country": "US"
+//     },
+//     {
+//       "id": 7505,
+//       "logo_path": "/837VMM4wOkODc1idNxGT0KQJlej.png",
+//       "name": "Marvel Entertainment",
+//       "origin_country": "US"
+//     }
+//   ],
+//   "production_countries": [
+//     {
+//       "iso_3166_1": "US",
+//       "name": "United States of America"
+//     }
+//   ],
+//   "release_date": "2022-03-30",
+//   "revenue": 161000000,
+//   "runtime": 104,
+//   "spoken_languages": [
+//     {
+//       "english_name": "English",
+//       "iso_639_1": "en",
+//       "name": "English"
+//     }
+//   ],
+//   "status": "Released",
+//   "tagline": "A new Marvel legend arrives.",
+//   "title": "Morbius",
+//   "video": false,
+//   "vote_average": 6.4,
+//   "vote_count": 1070
+// }
+
+
+
+// * TV
+
+// {
+//   "adult": false,
+//   "backdrop_path": "/vfWdZYGR5505zrCdgTHteFyARu3.jpg",
+//   "created_by": [
+//     {
+//       "id": 1147923,
+//       "credit_id": "5f970adf7719d700363e004a",
+//       "name": "Jeremy Slater",
+//       "gender": 2,
+//       "profile_path": "/fQKd49lOrhU80PQla8mmx3vSR20.jpg"
+//     }
+//   ],
+//   "episode_run_time": [
+//     47
+//   ],
+//   "first_air_date": "2022-03-30",
+//   "genres": [
+//     {
+//       "id": 10759,
+//       "name": "Action & Adventure"
+//     },
+//     {
+//       "id": 10765,
+//       "name": "Sci-Fi & Fantasy"
+//     },
+//     {
+//       "id": 9648,
+//       "name": "Mystery"
+//     }
+//   ],
+//   "homepage": "https://www.disneyplus.com/series/moon-knight/4S3oOF1knocS",
+//   "id": 92749,
+//   "in_production": false,
+//   "languages": [
+//     "en"
+//   ],
+//   "last_air_date": "2022-05-04",
+//   "last_episode_to_air": {
+//     "air_date": "2022-05-04",
+//     "episode_number": 6,
+//     "id": 3453975,
+//     "name": "Gods and Monsters",
+//     "overview": "With Layla tracking down Harrow, Marc, Steven, and Khonshu must return to Cairo to stop Ammit as she looks to threaten humanity once more.",
+//     "production_code": "",
+//     "runtime": 44,
+//     "season_number": 1,
+//     "still_path": "/owNx8ZoryZZzp6WYqmTV9Umf1bQ.jpg",
+//     "vote_average": 7.1,
+//     "vote_count": 15
+//   },
+//   "name": "Moon Knight",
+//   "next_episode_to_air": null,
+//   "networks": [
+//     {
+//       "name": "Disney+",
+//       "id": 2739,
+//       "logo_path": "/uzKjVDmQ1WRMvGBb7UNRE0wTn1H.png",
+//       "origin_country": "US"
+//     }
+//   ],
+//   "number_of_episodes": 6,
+//   "number_of_seasons": 1,
+//   "origin_country": [
+//     "US"
+//   ],
+//   "original_language": "en",
+//   "original_name": "Moon Knight",
+//   "overview": "When Steven Grant, a mild-mannered gift-shop employee, becomes plagued with blackouts and memories of another life, he discovers he has dissociative identity disorder and shares a body with mercenary Marc Spector. As Steven/Marc’s enemies converge upon them, they must navigate their complex identities while thrust into a deadly mystery among the powerful gods of Egypt.",
+//   "popularity": 2758.413,
+//   "poster_path": "/YksR65as1ppF2N48TJAh2PLamX.jpg",
+//   "production_companies": [
+//     {
+//       "id": 420,
+//       "logo_path": "/hUzeosd33nzE5MCNsZxCGEKTXaQ.png",
+//       "name": "Marvel Studios",
+//       "origin_country": "US"
+//     }
+//   ],
+//   "production_countries": [
+//     {
+//       "iso_3166_1": "US",
+//       "name": "United States of America"
+//     }
+//   ],
+//   "seasons": [
+//     {
+//       "air_date": "2022-03-30",
+//       "episode_count": 6,
+//       "id": 130604,
+//       "name": "Season 1",
+//       "overview": "",
+//       "poster_path": "/11keFudto4QrgrXChukexJwdHPe.jpg",
+//       "season_number": 1
+//     }
+//   ],
+//   "spoken_languages": [
+//     {
+//       "english_name": "English",
+//       "iso_639_1": "en",
+//       "name": "English"
+//     }
+//   ],
+//   "status": "Ended",
+//   "tagline": "Embrace the chaos.",
+//   "type": "Miniseries",
+//   "vote_average": 8.3,
+//   "vote_count": 872
+// }
